47f0ab4f16c949b29e8e54de01527e9905179bdd
TpL <tranphilong030201@gmail.com>
2023-01-30 10:36:58 +0700

    final-3

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,12 @@ var btns = document.getElementsByClassName('title');
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function () {
         var current = document.getElementsByClassName('active-title');
-        current[0].className = current[0].className.replace(
-            ' active-title',
-            ''
-        );
+        if(current.length != 0) {
+            current[0].className = current[0].className.replace(
+                ' active-title',
+                ''
+            );
+        }
         this.className += ' active-title';
     });
 }
